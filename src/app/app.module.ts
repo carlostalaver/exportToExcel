e425f11ad5e1daba1248d955d2f4a26f92734069
Carlos Talavera <carlostalaver@gmail.com>
2019-07-20 09:55:31 -0400

    Creacion del servicio que permite descargar archivo excel+

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import {MatTableModule} from '@angular/material/table';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatFormFieldModule
+} from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
